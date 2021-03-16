f5b13228f4e88913fb9efb4def471f63d9aaa8c1
Murat <40229902+Codirovshik@users.noreply.github.com>
2021-03-16 16:41:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,97 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+
 from pydantic import BaseModel
+from typing import List, Optional, Dict
 
-couriers = {}
-orders = {}
-
-
-class CouriersDataPost(BaseModel):
-    courier_id: int
-    courier_type: str
-    regions: list
-    working_hours: list
-
-
-class CouriersDataPatch(BaseModel):
-    courier_type: str
-    regions: list
-    working_hours: list
-
-
-class OrderDataPost(BaseModel):
-    order_id: int
-    weight: float
-    region: int
-    delivery_hours: list
 
 
 app = FastAPI()
 
+#couriers
+couriers = {}
 
-@app.get('/get_couriers')
-async def get_list():
+class Сouriers_post(BaseModel):
+    data: List = None
+
+
+class Сouriers_patch(BaseModel):
+    data: List = None
+
+
+@app.post('/couriers', response_model=Сouriers_post)
+async def post_query(data: Сouriers_post):
+    created_couriers = []
+    error_id = []
+    # сохранение курьера в системе
+    for item in data.data:
+        try:
+            couriers[item['courier_id']] = {
+                'courier_type': item['courier_type'],
+                'regions': item['regions'],
+                'working_hours': item['working_hours']
+            }
+
+            created_couriers.append({'id': item['courier_id']})
+        except Exception:
+            error_id.append({'id': item['courier_id']})
+    
+    if error_id:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"validation_error": {"couriers": error_id}})
+    else:
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content={"couriers": created_couriers})
+
+# TODO: не работает, нужно починить
+# @app.patch('/couriers/{courier_id}', response_model=Сouriers_patch)
+# async def patch_query(courier_id: int, data: Сouriers_patch):
+#     stored_item_data = couriers[courier_id]
+#     stored_item_model = Сouriers_patch(**stored_item_data)
+#     update_data = data.dict(exclude_unset=True)
+#     updated_item = stored_item_model.copy(update=update_data)
+#     couriers[courier_id] = jsonable_encoder(updated_item)
+#     return updated_item
+
+###############################################################################
+
+#orders
+
+orders = {}
+
+class Orders_post(BaseModel):
+    data: List = None
+
+
+@app.post('/orders', response_model=Orders_post)
+async def post_query(data: Orders_post):
+    created_orders = []
+    error_id = []
+    # сохранение курьера в системе
+    for item in data.data:
+        try:
+            orders[item['order_id']] = {
+                "weight": item["weight"],
+                "region": item["region"],
+                "delivery_hours": item["delivery_hours"]
+            }
+
+            created_orders.append({'id': item['order_id']})
+        except Exception:
+            error_id.append({'id': item['order_id']})
+    
+    if error_id:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"validation_error": {"orders": error_id}})
+    else:
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content={"orders": created_orders})
+
+
+
+@app.get('/couriers/get')
+async def get_query():
     return couriers
 
 
-# couriers api
-@app.post("/couriers")
-async def create_courier(data: dict):
-    for item in data['data']:
-        couriers[item['courier_id']] = [{
-            'courier_type': item['courier_type'],
-            'regions': item['regions'],
-            'working_hours': item['working_hours']
-        }]
-
-    all_id = len(couriers)
-    id_list = []
-    for index in range(all_id):
-        id_list.append({'id': index})
-    return 'HTTP 201 Created', {"couriers": id_list}
-
-
-# TODO: Не работает
-@app.patch('/couriers/{courier_id}', response_model=CouriersDataPatch)
-async def update_item(courier_id: str, data: CouriersDataPatch):
-    stored_item_data = couriers[courier_id]
-
-    stored_item_data[0].update({'courier_type': data.courier_type,
-                                'regions': data.regions, 'working_hours': data.working_hours})
-
-
-# orders api
-@app.get('/get_orders')
-async def get_list():
+@app.get('/orders/get')
+async def get_query():
     return orders
-
-
-@app.post("/orders")
-async def create_courier(data: OrderDataPost):
-    orders[data.order_id] = [{
-        'weight': data.weight,
-        'region': data.region,
-        'delivery_hours': data.delivery_hours
-    }]
-
-    all_id = len(orders)
-    id_list = []
-    for index in range(all_id):
-        id_list.append({'id': index})
-    return 'HTTP 201 Created', {"orders": id_list}
 
